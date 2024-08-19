6ea970309d59d08131fe36515493229293407925
verarr <verarr@fvnki.town>
2024-08-19 16:28:56 +0200

    refactor(osu_api): :truck: move TTLCachedDict class to osu api module
    i might extract this class into an external package in the future

--- osu_api.py
+++ osu_api.py
@@ -1,10 +1,11 @@
 import pickle
 import re
+from typing import Callable, Mapping, Never, TypeVar
 
 import osu
+from cachetools import TTLCache
 
 from .misc.constants import OsuBeatmapId, OsuUserId
-from .misc.utils import TTLCachedDict
 
 SECRETS_DIR: str = "./secrets"
 
@@ -40,6 +41,39 @@ def parse_beatmap_url(url: str) -> tuple[int, osu.GameModeStr, int]:
             )
     diff_id: int = int(re_match.group("diff_id"))
     return (set_id, mode, diff_id)
+
+
+K, V = TypeVar("K"), TypeVar("V")
+
+
+class TTLCachedDict(Mapping[K, V]):
+    _cache: TTLCache[K, V]
+    _get_func: Callable[[K], V]
+
+    def __init__(self, maxsize: int, ttl: int, get_func: Callable[[K], V]) -> None:
+        self._cache = TTLCache(maxsize=maxsize, ttl=ttl)
+        self._get_func = get_func
+
+    def __getitem__(self, key: K) -> V:
+        if key not in self._cache:
+            try:
+                self._cache[key] = self._get_func(key)
+            except Exception as e:
+                raise KeyError("Failed to retrieve value.") from e
+        return self._cache[key]
+
+    def __contains__(self, key: K) -> bool:
+        try:
+            _ = self.get(key)
+            return True
+        except KeyError:
+            return False
+
+    def __iter__(self) -> Never:
+        raise NotImplementedError
+
+    def __len__(self) -> Never:
+        raise NotImplementedError
 
 
 class CachedOsuClient:
