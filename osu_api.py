91525fee66f46f3907f1feec954cfa99fc5c2a7f
verarr <verarr@fvnki.town>
2024-09-01 13:02:00 +0200

    feat(osu_api): :loud_sound: add logging to osu api

--- osu_api.py
+++ osu_api.py
@@ -1,3 +1,4 @@
+import logging
 import pickle
 import re
 from typing import Callable, Mapping, Never, TypeVar
@@ -6,6 +7,9 @@ import osu
 from cachetools import TTLCache
 
 from misc.constants import OsuBeatmapId, OsuUserId
+
+logger = logging.getLogger("osuvs." + __name__)
+logger.setLevel(logging.DEBUG)
 
 _SECRETS_DIR: str = "./secrets"
 
@@ -19,6 +23,7 @@ except FileNotFoundError as e:
 
 
 def parse_beatmap_url(url: str) -> tuple[int, osu.GameModeStr, int]:
+    logger.debug(f"Parsing beatmap URL: {url}")
     re_match = re.search(
         r"/beatmapsets/(?P<set_id>\d+)#(?P<mode>[a-z]+)/(?P<diff_id>\d+)", url
     )
@@ -40,6 +45,7 @@ def parse_beatmap_url(url: str) -> tuple[int, osu.GameModeStr, int]:
                 "Invalid mode. Must be one of 'osu', 'taiko', 'fruits', 'mania'."
             )
     diff_id: int = int(re_match.group("diff_id"))
+    logger.debug(f"Parsed beatmap details: {set_id}, {modestr}, {diff_id}")
     return (set_id, mode, diff_id)
 
 
@@ -47,22 +53,30 @@ _K, _V = TypeVar("K"), TypeVar("V")
 
 
 class _TTLCachedDict(Mapping[_K, _V]):
+    _log: logging.Logger
     _cache: TTLCache[_K, _V]
     _get_func: Callable[[_K], _V]
 
     def __init__(self, maxsize: int, ttl: int, get_func: Callable[[_K], _V]) -> None:
         self._cache = TTLCache(maxsize=maxsize, ttl=ttl)
         self._get_func = get_func
+        self._log = logging.getLogger(
+            logger.name + ".client." + self.__class__.__name__
+        )
 
     def __getitem__(self, key: _K) -> _V:
         if key not in self._cache:
+            self._log.debug(f"Cache miss for key {key}")
             try:
                 self._cache[key] = self._get_func(key)
             except Exception as e:
                 raise KeyError("Failed to retrieve value.") from e
+        else:
+            self._log.debug(f"Cache hit for key {key}")
         return self._cache[key]
 
     def __contains__(self, key: _K) -> bool:
+        self._log.debug(f"Checking availability of key {key}")
         try:
             _ = self.get(key)
             return True
@@ -70,14 +84,17 @@ class _TTLCachedDict(Mapping[_K, _V]):
             return False
 
     def __iter__(self) -> Never:
+        self._log.error(f"Cannot iterate over api queries")
         raise NotImplementedError
 
     def __len__(self) -> Never:
+        self._log.error(f"Cannot get length of api queries")
         raise NotImplementedError
 
 
 class _CachedOsuClient:
     _client: osu.Client
+    _log: logging.Logger
     users: _TTLCachedDict[tuple[OsuUserId | str, osu.GameModeStr | None], osu.User]
     beatmaps: _TTLCachedDict[OsuBeatmapId, osu.Beatmap]
 
@@ -106,6 +123,8 @@ class _CachedOsuClient:
             ttl=60 * 15,
             get_func=lambda x: self._client.get_beatmap(int(x)),
         )
+        self._log = logging.getLogger(logger.name + ".client")
+        self._log.debug("Initialized cached osu! client")
 
 
 client = _CachedOsuClient(
@@ -113,3 +132,5 @@ client = _CachedOsuClient(
         _OSU_API_DETAILS["client_id"], _OSU_API_DETAILS["client_secret"], None
     )
 )
+
+logger.info("osu! API client initialized successfully.")
